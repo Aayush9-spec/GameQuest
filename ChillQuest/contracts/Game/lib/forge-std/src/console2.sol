f5a291925eaab63331480c2865bb5a38bd4439b8
Aayush Kumar Singh <2k24.cs1a.2414142@gmail.com>
2025-11-01 21:22:28 +0530

    commit

---
+++ ChillQuest/contracts/Game/lib/forge-std/src/console2.sol
@@ -0,0 +1,4 @@
+
+pragma solidity >=0.4.22 <0.9.0;
+
+import {console as console2} from "./console.sol";
---
+++ ChillQuest/contracts/Game/lib/socket-protocol/contracts/common/Errors.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.13;
+
+error NotAuthorized();
+error NotBridge();
+error NotSocket();
+error ConnectorUnavailable();
+error InvalidTokenContract();
+error ZeroAddressReceiver();
+error ZeroAddress();
+error ZeroAmount();
+error InsufficientFunds();
+error InvalidSigner();
+error InvalidFunction();
